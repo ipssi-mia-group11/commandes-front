528c4f1e8932446251ee6da52dc6fe3a7d611710
Thomas Léveillé <leveille.thomas77@gmail.com>
2023-10-18 16:04:44 +0200

    feat: app router

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
+import {
+    Link,
+    Outlet,
+} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return <>
+        <header>
+            <nav>
+                <Link to={'/'}>Home</Link>
+                <Link to={'/about'}>About</Link>
+                <Link to={'/users'}>Users</Link>
+            </nav>
+        </header>
+        <main>
+            <Outlet />
+        </main>
+        <footer>
+        </footer>
+    </>;
 }
 
 export default App;
